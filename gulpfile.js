260ebbf088de8f4f8d0f8b6b42bb7d7d7943386a
Bartek Igielski <igloczek@gmail.com>
2016-04-05 00:07:18 +0200

    CSS and SCSS linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,59 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-sass'),
-    plumber = require('gulp-plumber'),
-    csslint = require('gulp-csslint'),
-    gutil   = require('gulp-util');
+var gulp      = require('gulp'),
+    sass      = require('gulp-sass'),
+    plumber   = require('gulp-plumber'),
+    gutil     = require('gulp-util'),
+    postcss   = require('gulp-postcss'),
+    reporter  = require('postcss-reporter'),
+    stylelint = require('stylelint'),
+    sassLint  = require('gulp-sass-lint');
 
-var cssLintSettings = {
-    'adjoining-classes'          : false,
-    'box-model'                  : false,
-    'box-sizing'                 : false,
-    'compatible-vendor-prefixes' : true,
-    'empty-rules'                : true,
-    'display-property-grouping'  : false,
-    'duplicate-background-images': false,
-    'duplicate-properties'       : false,
-    'fallback-colors'            : false,
-    'floats'                     : false,
-    'font-faces'                 : true,
-    'font-sizes'                 : false,
-    'gradients'                  : true,
-    'ids'                        : false,
-    'import'                     : true,
-    'important'                  : false,
-    'known-properties'           : true,
-    'outline-none'               : true,
-    'overqualified-elements'     : false,
-    'qualified-headings'         : false,
-    'regex-selectors'            : false,
-    'shorthand'                  : false,
-    'selector-max'               : false, // IE max selectors quantity error
-    'selector-max-approaching'   : false, // IE max selectors quantity warning
-    'star-property-hack'         : true,
-    'text-indent'                : true,
-    'underscore-property-hack'   : true,
-    'unique-headings'            : false,
-    'universal-selector'         : false,
-    'unqualified-attributes'     : false,
-    'vendor-prefix'              : false,
-    'zero-units'                 : true
-};
-
-// css lint custom formater
-function customReporter(file) {
-    gutil.log(
-        gutil.colors.cyan(file.csslint.errorCount) + ' errors in '
-        + gutil.colors.magenta(file.path)
-    );
-
-    file.csslint.results.forEach(function(result) {
-        if (result.error.type === 'warning') {
-            gutil.log(
-                gutil.colors.yellow.bold('[Warining]')
-                + gutil.colors.green(' Line: ' + result.error.line)
-                + gutil.colors.cyan(' Column: ' + result.error.col) + ' '
-                + gutil.colors.magenta(result.error.message) + ' '
-                + gutil.colors.gray(result.error.rule.desc) + ' '
-                + gutil.colors.red('Browsers: ' + result.error.rule.browsers)
-            );
+// Rewrite to YAML and then move to .stylelintrc, b/c JSON don't support comments
+var cssStylelintConfig = {
+        "rules": {
+            "color-no-invalid-hex": true,
+            "time-no-imperceptible": true,
+            // "declaration-block-no-duplicate-properties": true, // to do - too many errors
+            // "declaration-block-no-ignored-properties": true, // to enable in >=5.3.0
+            "declaration-block-no-shorthand-property-overrides": true,
+            "block-no-empty": true,
+            // "selector-no-attribute": true, // to do - too many errors
+            "selector-no-id": true,
+            "no-unknown-animations": true,
+            "no-browser-hacks": [true, {"browsers": "> 1%, last 2 versions, not ie < 11, not OperaMini >= 5.0"}],
+            // "no-unsupported-browser-features": [true, {"browsers": "> 1%, last 2 versions, not ie < 11, not OperaMini >= 5.0"}] // to do - too many errors
         }
-        else {
-            gutil.log(
-                gutil.colors.red.bold('[' + result.error.type + ']')
-                + gutil.colors.green(' Line: ' + result.error.line)
-                + gutil.colors.cyan(' Column: ' + result.error.col) + ' '
-                + gutil.colors.magenta(result.error.message) + ' '
-                + gutil.colors.gray(result.error.rule.desc) + ' '
-                + gutil.colors.red('Browsers: ' + result.error.rule.browsers)
-            );
-        }
-    });
-}
+    };
 
 gulp.task('default', () => {
-    gulp.watch('**/*.scss', ['sass']);
+    gulp.watch(['**/*.scss','!node_modules/**'], ['sass']);
 });
 
 gulp.task('sass', () => {
     return gulp.src('web/css/*.scss')
-        .pipe(sass({
-            outputStyle   : 'expanded',
-            sourceComments: true
-        }).on('error', sass.logError))
+        .pipe(
+            sass({
+                outputStyle   : 'expanded',
+                sourceComments: true
+            })
+            .on('error', sass.logError)
+        )
         .pipe(gulp.dest('web/css'));
 });
 
-gulp.task('lint', () => {
+gulp.task('css-lint', () => {
     return gulp.src('web/css/*.css')
-        .pipe(csslint(cssLintSettings))
-        .pipe(csslint.reporter(customReporter))
-        .pipe(csslint.reporter('fail'));
+        .pipe(postcss([
+            stylelint(cssStylelintConfig),
+            reporter({
+                clearMessages: true,
+                throwError   : true
+            })
+        ]));
+});
+
+gulp.task('scss-lint', () => {
+    return gulp.src(['**/*.scss','!node_modules/**'])
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError());
 });
